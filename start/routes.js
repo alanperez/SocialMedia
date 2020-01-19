9e07eab83c58d9e84a4d072049d44b602ba455e8
Alan Perez <alanp9six@gmail.com>
2020-01-18 23:45:04 -0600

    added get req

--- start/routes.js
+++ start/routes.js
@@ -16,4 +16,5 @@
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 
-Route.on('/').render('pages/home')
+// Route.on('/').render('pages/home')
+Route.get('/', 'PageController.home')
