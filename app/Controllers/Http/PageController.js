1a6e5d98e843c4cf9223c6ae404161f0718bba6d
Alan Perez <alanp9six@gmail.com>
2020-01-19 19:48:01 -0600

    welcome render added

--- app/Controllers/Http/PageController.js
+++ app/Controllers/Http/PageController.js
@@ -4,6 +4,10 @@ class PageController {
   async home({ res, req, view}) {
     return view.render('pages/home')
   }
+
+  async welcome({ res, req, view}) {
+    return view.render('pages/welcome')
+  }
 }
 
 module.exports = PageController
